5f7f410e1f85e574bd973b75a919b09e57baf572
Chris Dillon <squarism@gmail.com>
2015-01-29 11:15:53 -0800

    add working parallax system stage

---
+++ src/parallax_system.rb
@@ -0,0 +1,69 @@
+class ParallaxSystem
+  attr_accessor :rate
+  attr_reader :actors
+
+  def initialize(stage:, width:, height:, actor_name:, x:, y:, layer:)
+    @width = width
+    @height = height
+    @origin_x = x
+    @origin_y = y
+    @layer = layer
+    @actor_name = actor_name
+    @stage = stage
+    @rate = 0.10
+
+    spawn_initial_actors
+  end
+
+  def spawn_initial_actors
+    @actors = []
+    @actors << @stage.create_actor(@actor_name, x:left_spawn_point,  y:@origin_y, width: @width, height: @height, layer: @layer)
+    @actors << @stage.create_actor(@actor_name, x:@origin_x,         y:@origin_y, width: @width, height: @height, layer: @layer)
+    @actors << @stage.create_actor(@actor_name, x:right_spawn_point, y:@origin_y, width: @width, height: @height, layer: @layer)
+  end
+
+  def left_spawn_point
+    @origin_x - @width
+  end
+
+  def right_spawn_point
+    @origin_x + @width
+  end
+
+  def start(tween_manager)
+    @actors.each do |actor|
+      start_actor(tween_manager, actor)
+    end
+  end
+
+  def start_actor(tween_manager, actor)
+    destination_x = actor.x - @width * 3.0
+    distance_x = (actor.x - destination_x).abs
+    @stage.tween_manager.tween_properties actor,
+    {x: destination_x, y:actor.y}, distance_x / @rate, Tween::Linear
+  end
+
+  def respawn_parallax_actor
+    puts "respawning..."
+    spawn_x = @actors.first.x + @width * 3.0
+    respawn = @actors.shift
+    respawn.x = spawn_x
+    respawn
+  end
+
+  # when first texture is halfway off the screen, spawn another one
+  def check_for_respawn(tween_manager)
+    if @actors.first.x < @origin_x - @width
+      puts "I need to respawn."
+      actor = respawn_parallax_actor
+      start_actor(tween_manager, actor)
+      @actors << actor
+    end
+
+    if @actors.first.x <= -(@width)
+      dead = @actors.shift
+      dead.remove
+    end
+  end
+
+end
---
+++ src/stages/system.rb
@@ -0,0 +1,48 @@
+# this stage is a working parallax system but zoomed out for demo purposes.
+define_stage :system do
+  requires :tween_manager
+
+  curtain_up do
+    @stagehand = create_actor :stagehand
+    @border_enabled = false
+    center_x = viewport.width / 2
+    center_y = viewport.height / 2
+    fake_viewport_width = viewport.width / 4
+    fake_viewport_height = viewport.height / 4
+    @fake_viewport = create_actor :fake_viewport,
+      x:center_x, y:center_y, width:fake_viewport_width+2, height:fake_viewport_height+2
+
+    # gamebox doesn't scale images for you with width and height
+    # so I'm just hardcoding the width to the save of the screen
+    # be sure to size your images beforehand
+    sky = ParallaxSystem.new(stage:self,
+      width:fake_viewport_width, height:fake_viewport_height,
+      actor_name: :sky_small, x:center_x, y:center_y, layer:1
+    )
+    sky.start tween_manager
+
+    director.when :update do |time|
+      sky.check_for_respawn(tween_manager)
+    end
+
+    @stagehand.when(:remove_borders) do
+      @border_enabled = false
+      sky.each do |actor|
+        actor.border_enabled = @border_enabled
+      end
+    end
+
+    input_manager.reg :down, K_SPACE do
+      @stagehand.progression_counter += 1
+
+      case @stagehand.progression_counter
+      when 1
+        @stagehand.fire :remove_borders
+      else
+        fire :next_stage
+      end
+    end
+
+  end
+
+end
